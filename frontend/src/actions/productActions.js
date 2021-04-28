3b9555871038827382c8a2e490789e35ad3d7442
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2021-04-28 19:20:39 +0545

    bringing redux into homescreen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,7 +4,7 @@ import {
 	PRODUCT_LIST_SUCCESS,
 	PRODUCT_LIST_FAIL,
 } from '../constants/productConstants';
-const listProducts = () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
 	try {
 		dispatch({ type: PRODUCT_LIST_REQUEST });
 		const { data } = await axios.get('api/products/');
