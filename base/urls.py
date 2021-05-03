10674b6c35e7229a9a100afba817402b1b9dd657
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2021-05-03 13:51:02 +0545

    JWT installation and setup

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
 from . import views as base_views
 
 urlpatterns = [
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', base_views.getRoutes, name='routes'),
     path('products/', base_views.getProducts, name='products'),
     path('products/<str:pk>', base_views.getProduct, name='product'),
