ba6c89018c487eaf0e1aed3bbd7330eecae31934
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2021-05-31 20:46:46 +0545

    User Register Reducer, Action & Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,22 +8,24 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 function App() {
-	return (
-		<Router>
-			<Header />
-			<main className='py-3'>
-				<Container>
-					<Route path='/' component={HomeScreen} exact />
-					<Route path='/login' component={LoginScreen} />
-					<Route path='/product/:id' component={ProductScreen} />
-					<Route path='/cart/:id?' component={CartScreen} />
-				</Container>
-			</main>
-			<Footer />
-		</Router>
-	);
+    return (
+        <Router>
+            <Header />
+            <main className='py-3'>
+                <Container>
+                    <Route path='/' component={HomeScreen} exact />
+                    <Route path='/login' component={LoginScreen} />
+                    <Route path='/register' component={RegisterScreen} />
+                    <Route path='/product/:id' component={ProductScreen} />
+                    <Route path='/cart/:id?' component={CartScreen} />
+                </Container>
+            </main>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Form, Row, Col } from 'react-bootstrap';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import FormContainer from '../components/FormContainer';
-import { login } from '../actions/userActions';
+import { register } from '../actions/userActions';
 
-function LoginScreen({ location, history }) {
+function RegisterScreen({ location, history }) {
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [message, setMessage] = useState('');
 
     const dispatch = useDispatch();
 
     const redirect = location.search ? location.search.split('=')[1] : '/';
 
-    const userLogin = useSelector((state) => state.userLogin);
-    const { error, loading, userInfo } = userLogin;
+    const userRegister = useSelector((state) => state.userRegister);
+    const { error, loading, userInfo } = userRegister;
 
     useEffect(() => {
         if (userInfo) {
@@ -27,20 +30,37 @@ function LoginScreen({ location, history }) {
 
     const submitHandler = (e) => {
         e.preventDefault();
-        dispatch(login(email, password));
+
+        if (password !== confirmPassword) {
+            setMessage('Passwords do not match');
+        } else {
+            dispatch(register(name, email, password));
+        }
     };
 
     return (
         <FormContainer>
-            <h1>Sign In</h1>
-
+            <h1>Register</h1>
+            {message && <Message variant='danger'>{message}</Message>}
             {error && <Message variant='danger'>{error}</Message>}
             {loading && <Loader />}
 
             <Form onSubmit={submitHandler}>
+                <Form.Group controlId='name'>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        required
+                        type='name'
+                        placeholder='Enter Name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </Form.Group>
+
                 <Form.Group controlId='email'>
                     <Form.Label>Email Address</Form.Label>
                     <Form.Control
+                        required
                         type='email'
                         placeholder='Enter Email'
                         value={email}
@@ -51,26 +71,36 @@ function LoginScreen({ location, history }) {
                 <Form.Group controlId='password'>
                     <Form.Label>Password</Form.Label>
                     <Form.Control
+                        required
                         type='password'
                         placeholder='Enter Password'
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
                 </Form.Group>
+
+                <Form.Group controlId='passwordConfirm'>
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control
+                        required
+                        type='password'
+                        placeholder='Confirm Password'
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                </Form.Group>
                 <Button type='submit' variant='primary'>
-                    Sign In
+                    Register
                 </Button>
             </Form>
             <Row className='py-3'>
                 <Col>
-                    New Customer?{' '}
+                    Have an account?{' '}
                     <Link
                         to={
-                            redirect
-                                ? `/register?redirect=${redirect}`
-                                : '/register'
+                            redirect ? `/login?redirect=${redirect}` : '/login'
                         }>
-                        Register
+                        Login
                     </Link>
                 </Col>
             </Row>
@@ -78,4 +108,4 @@ function LoginScreen({ location, history }) {
     );
 }
 
-export default LoginScreen;
+export default RegisterScreen;
