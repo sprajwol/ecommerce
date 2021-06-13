67bd17ed8d371ab4a8e663bf0949d14cf989532f
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2021-06-13 17:56:22 +0545

    User List Reducer, Action & Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
+import UserListScreen from './screens/UserListScreen';
 
 function App() {
     return (
@@ -31,6 +32,8 @@ function App() {
                     <Route path='/order/:id' component={OrderScreen} />
                     <Route path='/product/:id' component={ProductScreen} />
                     <Route path='/cart/:id?' component={CartScreen} />
+
+                    <Route path='/admin/userlist' component={UserListScreen} />
                 </Container>
             </main>
             <Footer />
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -50,6 +50,26 @@ function Header() {
                                     </Nav.Link>
                                 </LinkContainer>
                             )}
+                            {console.log('userInfo', userInfo)}
+                            {userInfo && userInfo.isAdmin && (
+                                <NavDropdown title='Admin' id='adminmenue'>
+                                    <LinkContainer to='/admin/userlist'>
+                                        <NavDropdown.Item>
+                                            Users
+                                        </NavDropdown.Item>
+                                    </LinkContainer>
+                                    <LinkContainer to='/admin/productlist'>
+                                        <NavDropdown.Item>
+                                            Products
+                                        </NavDropdown.Item>
+                                    </LinkContainer>
+                                    <LinkContainer to='/admin/orderlist'>
+                                        <NavDropdown.Item>
+                                            Orders
+                                        </NavDropdown.Item>
+                                    </LinkContainer>
+                                </NavDropdown>
+                            )}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
